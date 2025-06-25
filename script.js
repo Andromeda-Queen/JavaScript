6f2a66c1a65830f60fb7a98674c6a3776079ecbc
Andromeda-Queen <163483823+Andromeda-Queen@users.noreply.github.com>
2025-06-25 20:18:48 -0300

    1.1

---
+++ script.js
@@ -0,0 +1,3 @@
+alert("Comando para mostrar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga esse padrão em todo o código");
+alert("Ele é limitado ao clique no ok pelo usuário");
+alert("Não é possível configurar o seu tempo de permanência na tela ");
