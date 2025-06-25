448828f9e406c963b1483ff55a7fc36910652e87
Andromeda-Queen <163483823+Andromeda-Queen@users.noreply.github.com>
2025-06-25 20:37:30 -0300

    1.3

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 alert("Comando para mostrar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga esse padrão em todo o código");
-let numero = 25;
 let chute;
 let idade;
 let nome;
 
 console.log (numero);
+
+let entrada = prompt("Digite um número inteiro");
+let numero = parseInt(entrada);
+
+if (Number.isInteger(numero)){
+    console.log("Obrigado! Voce digitou um inteiro.");
+} else {
+    console.log("Por favor, digite um número inteiro válido.")
+}
