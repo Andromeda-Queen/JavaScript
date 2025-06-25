56d7cb1e52d50f4447f0a48e48f167c711e88257
Andromeda-Queen <163483823+Andromeda-Queen@users.noreply.github.com>
2025-06-25 20:25:06 -0300

    1.2

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 alert("Comando para mostrar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga esse padrão em todo o código");
 alert("Ele é limitado ao clique no ok pelo usuário");
 alert("Não é possível configurar o seu tempo de permanência na tela ");
+let numero = 25;
+let chute;
+let idade;
+let nome;
+
+console.log (numero);
